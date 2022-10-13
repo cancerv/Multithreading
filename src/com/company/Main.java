b82071feeba1e65d504db5a210fcbb204da9e3d9
Veronica <veronicaegorova13@gmail.com>
2022-10-13 18:42:37 +0700

    fix

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,18 +6,13 @@ public class Main {
         System.out.println("Создаю потоки...");
         MyThread myThread = new MyThread();
         ThreadGroup mainGroup = new ThreadGroup("main group");
-        final Thread thread1 = new Thread(mainGroup, myThread);
-        thread1.start();
-        final Thread thread2 = new Thread(mainGroup, myThread);
-        thread2.start();
-        final Thread thread3 = new Thread(mainGroup, myThread);
-        thread3.start();
-        final Thread thread4 = new Thread(mainGroup, myThread);
-        thread4.start();
+        for (int i = 0; i < 4; i++) {
+            final Thread thread = new Thread(mainGroup, myThread);
+            thread.start();
+        }
 
         Thread.sleep(15000);
         System.out.println("Завершаю все потоки.");
         mainGroup.interrupt();
-
     }
 }
